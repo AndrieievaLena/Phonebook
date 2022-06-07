a1ef32ffac01f0b68956d8261cc6983b672aa4fd
AndrieievaLena <elenakovalevamod@gmail.com>
2022-06-07 19:12:46 +0300

    Changed App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,49 @@
-export const App = () => {
+import {useState} from 'react'
+import React from 'react';
+
+function App() {
+  const [entries, setEntries] = useState([]);
+  const addEntryToPhonebook = (entry) => {
+  };
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <EntryForm />
+      <DisplayEntries />
     </div>
   );
+
+
 };
+export default App;
+
+  function EntryForm() {
+    const [firstName, setFirstName] = useState("");
+    const [midleName, setMidleName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const handleSubmit=(e) => {
+      e.preventDefault();
+    }
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="firstName" id="firstName" placeholder='First Name'
+        value={firstName} onChange={(e)=> setFirstName[e.target.value]}/>
+        <input type="text" name="midleName" id="midleName" placeholder='Midle Name'
+        value={midleName} onChange={(e)=> setMidleName[e.target.value]}/> 
+        <input type="text" name="lastName" id="lastName" placeholder='Last Name'
+        value={lastName} onChange={(e)=> setLastName[e.target.value]}/>
+        <input type="text" name="phoneNumber" id="phoneNumber" placeholder='Phone number'
+        value={phoneNumber} onChange={(e)=> setPhoneNumber[e.target.value]}/>
+      <button type='submit'>Submit</button>
+      </form>
+      
+    );
+  };
+
+  function DisplayEntries() {
+    return (
+      <tablet>
+
+      </tablet>
+    );
+  }
